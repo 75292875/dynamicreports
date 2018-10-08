7fd69075d82312e142b51e7eaab911a90acfcafe
Edwin Njeru <mailnjeru@gmail.com>
2018-10-08 18:01:23 +0300

    :arrow_heading_up: Add module for core maven module

---
+++ dynamicreports-core/src/main/java/module-info.java
@@ -0,0 +1,31 @@
+
+module dynamicreports {
+
+    requires java.desktop;
+    requires java.sql;
+    requires commons.lang3;
+    requires jasperreports;
+    requires jfreechart;
+    requires java.xml.bind;
+
+    exports net.sf.dynamicreports.design.base;
+    exports net.sf.dynamicreports.design.constant;
+    exports net.sf.dynamicreports.design.definition;
+    exports net.sf.dynamicreports.design.exception;
+    exports net.sf.dynamicreports.design.transformation;
+    exports net.sf.dynamicreports.jasper.base;
+    exports net.sf.dynamicreports.jasper.builder;
+    exports net.sf.dynamicreports.jasper.constant;
+    exports net.sf.dynamicreports.jasper.definition;
+    exports net.sf.dynamicreports.jasper.exception;
+    exports net.sf.dynamicreports.jasper.transformation;
+    exports net.sf.dynamicreports.report.base;
+    exports net.sf.dynamicreports.report;
+    exports net.sf.dynamicreports.report.builder;
+    exports net.sf.dynamicreports.report.component;
+    exports net.sf.dynamicreports.report.constant;
+    exports net.sf.dynamicreports.report.datasource;
+    exports net.sf.dynamicreports.report.defaults;
+    exports net.sf.dynamicreports.report.definition;
+    exports net.sf.dynamicreports.report.exception;
+}
